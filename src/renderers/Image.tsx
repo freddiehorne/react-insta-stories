d2296c5c75b9008bd2bc61dcc697703a73179f56
Freddie Horne <freddie@letsdothis.com>
2022-08-17 11:49:24 +0100

    it works!!!

--- src/renderers/Image.tsx
+++ src/renderers/Image.tsx
@@ -5,7 +5,11 @@ import { Pause, Play } from "./wrappers/icons";
 import WithHeader from "./wrappers/withHeader";
 import WithSeeMore from "./wrappers/withSeeMore";
 
+import useMedia from "../hooks";
+
 export const renderer: Renderer = ({ story, action, isPaused, config }) => {
+  const isMobile = useMedia("(max-width: 768px)");
+
   const [loaded, setLoaded] = React.useState(false);
   const { width, height, loader, storyStyles } = config;
   let computedStyles = {
@@ -30,7 +34,18 @@ export const renderer: Renderer = ({ story, action, isPaused, config }) => {
     <WithHeader story={story} globalHeader={config.header}>
       <WithSeeMore story={story} action={action}>
         <div>
-          <div style={styles.buttonsContainer}>
+          <div
+            style={{
+              position: "absolute" as const,
+              bottom: isMobile ? "20px" : "40px",
+              right: isMobile ? "20px" : "30px",
+              display: "flex",
+              flexDirection: "column" as const,
+              justifyContent: "center",
+              zIndex: 1000,
+              height: "64px",
+            }}
+          >
             {isPaused ? (
               <div style={styles.buttonCircle} onClick={onPlaying}>
                 <div style={{ padding: "5px 0 0 5px", margin: "6px 0 0 6px" }}>
@@ -83,16 +98,6 @@ const styles = {
     maxWidth: "100%",
     maxHeight: "100%",
     margin: "auto",
-  },
-  buttonsContainer: {
-    position: "absolute" as const,
-    bottom: "40px",
-    right: "30px",
-    display: "flex",
-    flexDirection: "column" as const,
-    justifyContent: "center",
-    zIndex: 1000,
-    height: "64px",
   },
   buttonCircle: {
     width: "64px",
