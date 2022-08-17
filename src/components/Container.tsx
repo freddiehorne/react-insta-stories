15c9a440235e8fbcad51ebbf9ad6c8badd6ba67c
Freddie Horne <freddie@letsdothis.com>
2022-08-17 15:26:50 +0100

    box shadow

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -148,6 +148,7 @@ export default function () {
         background: "#111",
         position: "relative" as const,
         borderRadius: isMobile ? "0px" : "16px",
+        boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
         ...{ width, height },
       }}
     >
